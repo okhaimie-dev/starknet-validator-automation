d0423fc6dd4535f8df7e0b1611816a11e5e79c55
KC Jones <115911031+kcjones91@users.noreply.github.com>
2025-08-21 17:45:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ install_dependencies_fedora() {
   
   echo "Installing Docker for Fedora..."
   sudo dnf -y install dnf-plugins-core
-  sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+  sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
   sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo systemctl enable --now docker
   sudo usermod -aG docker $USER
